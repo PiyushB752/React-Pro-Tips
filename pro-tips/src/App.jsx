65e2c5e1ebeba95b7ff854856d1f672fcbbfcb19
Piyush Bachani <Piyush>
2024-01-31 17:44:11 +0530

    Done

---
+++ pro-tips/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter , Route, Routes, Link } from 'react-router-dom';
+import './App.css';
+import Kalvium from './components/kalvium';
+import RegistrationForm from './components/registrationForm';
+import Contacts from './components/contacts';
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <nav>
+          <Link to='/' className='Link'>Kalvium❤️</Link>
+          <div className='links'>
+          <Link to='/contacts' className='Link'>Contact</Link>
+          <Link to='/registrationForm' className='Link'>Registration</Link>
+          </div>
+          <Routes>
+            <Route path="/" element={<Kalvium />} />
+            <Route path="/contacts" element={<Contacts/>}/>
+            <Route path="/registrationForm" element={<RegistrationForm/>} />
+          </Routes>
+        </nav>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
